2dc2ffe2c9477b2f16e424803c2939767c0ef77e
Igor <Igor_Khozov>
2023-08-22 18:37:14 +0500

    9 занятие

--- advertisment/app_advertisment/admin.py
+++ advertisment/app_advertisment/admin.py
@@ -4,10 +4,11 @@ from .models import Advertisement
 # Register your models here.
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'created_date','updated_date', 'auction']
+    list_display = ['id', 'title', 'description', 'price', 'created_date','updated_date','html_image', 'auction']
     list_filter = ['auction', 'created_at']
     actions = ['make_auction_as_false', 'make_auction_as_true']
     search_fields = ['title']
+
 
     @admin.action(description='Убрать возможность торга')
     def make_auction_as_false(self, request, queryset):
--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils import timezone
+from django.contrib.auth import get_user_model
 
-
+User= get_user_model()
 class Advertisement(models.Model):
     title = models.CharField('Заголовок', max_length=128)
     description = models.TextField('Описание')
@@ -12,6 +13,8 @@ class Advertisement(models.Model):
     auction = models.BooleanField('Торг', help_text='Отъметьте, если торг уместен')
     created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
     updated_at = models.DateTimeField('Дата и время изменения', auto_now=True)
+    user= models.ForeignKey (User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    image= models.ImageField('Изображение',upload_to='advertisements/')
 
     @admin.display(description='Дата создания')
     def created_date(self):
@@ -30,6 +33,13 @@ class Advertisement(models.Model):
                 '<span style="color: red; font-weight: bold;">Сегодня в {}</span>', updated_time
             )
         return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='Фотография')
+    def html_image(self):
+        if self.image:
+            return format_html(
+                '<img src="{url}" style="max-width: 80px; max-height: 80px;"', url=self.image.url
+            )
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
